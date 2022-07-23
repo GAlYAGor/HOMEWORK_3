159c0cd43b77f68eb4011019c63992c3bc5dad97
Galina Gorbunova <galinagorbunova542@gmail.com>
2022-07-23 19:45:46 +0400

    work 3

---
+++ work_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов 
+// чисел от 1 до N.
+
+Console.Write("Введите число:");
+int N = int.Parse(Console.ReadLine());
+{ 
+    Console.WriteLine("Число" + "\t" + "Куб");
+    for(int i = 1; i <= N; i++)
+    {
+        Console.WriteLine(i + "\t" + i*i*i);
+    }
+}    
+Console.ReadLine();
+  
